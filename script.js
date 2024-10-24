a4adc82bfb0e02d4da97901d39502cbd74c1c93a
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-10-24 14:12:02 +0100

    Added the clipBoard function and included code to help copied change back to copy

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const coupon = document.querySelector(".coupon")
 const copyText = (e) => {
   e.preventDefault()
 
+  navigator.clipboard.writeText(coupon.value).then(() => {
+    btn.textContent = "Copied!!!";
+    setTimeout(() =>{
   
+  btn.textContent = "Copy"
+    }, 3000);
+    
+  })
 
-setTimeout(() =>{
-  btn.textContent = "Copied!"
-  
-}, 3000);
+
 }
 
 btn.addEventListener("click", copyText)
