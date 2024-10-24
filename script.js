82ea23ed7d86a2b9b52b51813ca4e3e8f3cbb34d
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-10-24 13:53:56 +0100

    Removed depricated execCommand and changed 300  back to 3000

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const coupon = document.querySelector(".coupon")
 const copyText = (e) => {
   e.preventDefault()
 
-  coupon.select()
-  coupon.setSelectionRange(0, 99999)
-document.execCommand("copy")
+  
 
 setTimeout(() =>{
   btn.textContent = "Copied!"
   
-}, 300);
+}, 3000);
 }
 
 btn.addEventListener("click", copyText)
