761e2fcd9c9e0480f6d56ebb2898c6246e05c519
Moyo Oyeniyi <moyosoreoluwaoyeniyi@gmail.com>
2024-10-24 13:47:35 +0100

    Added HTML, CSS and JavaScript files

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn")
+const coupon = document.querySelector(".coupon")
+
+
+
+const copyText = (e) => {
+  e.preventDefault()
+
+  coupon.select()
+  coupon.setSelectionRange(0, 99999)
+document.execCommand("copy")
+
+setTimeout(() =>{
+  btn.textContent = "Copied!"
+  
+}, 3000);
+}
+
+btn.addEventListener("click", copyText)
